00b228460452501340fe5ed2be0ffa28da711770
lowfreq2010-home <vns.devios@gmail.com>
2021-02-19 09:13:57 +0300

    added scrolling image in scrollview if it is too big

--- skywebprotest/skywebprotest/UseCases/MainScreen/View/MainCVCell.swift
+++ skywebprotest/skywebprotest/UseCases/MainScreen/View/MainCVCell.swift
@@ -21,10 +21,29 @@ class MainCVCell: UICollectionViewCell {
             contentView.topAnchor.constraint(equalTo: topAnchor),
             contentView.bottomAnchor.constraint(equalTo: bottomAnchor)
         ])
+        
+        scrollerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            scrollerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            scrollerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            scrollerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false // Make sure constraints won't interfere with autoresizing mask
+        NSLayoutConstraint.activate(
+            [
+                imageView.leftAnchor.constraint(equalTo: scrollerView.leftAnchor),    // attaching to the left
+                imageView.topAnchor.constraint(equalTo: scrollerView.topAnchor),      // attaching to the top
+                imageView.rightAnchor.constraint(equalTo: scrollerView.rightAnchor),  // attaching to the right
+                imageView.bottomAnchor.constraint(equalTo: scrollerView.bottomAnchor) // attaching to the bottom
+            ]
+        )
     }
     
     func configure(with viewModel: ImageCellViewModel) {
         guard let img = viewModel.image else {return}
         self.imageView.image = img
+        self.scrollerView.contentSize = img.size
     }
 }
