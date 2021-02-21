c1b177b02a844a313909f74e65a25c35c4d1463c
lowfreq2010-home <vns.devios@gmail.com>
2021-02-21 09:49:15 +0300

    Revert "large images downloaded and shown"
    This reverts commit 5c5eabeb2e7275c270efc94cbddd8fdc258726db.

--- skywebprotest/skywebprotest/UseCases/MainScreen/View/MainCVCell.swift
+++ skywebprotest/skywebprotest/UseCases/MainScreen/View/MainCVCell.swift
@@ -16,32 +16,20 @@ class MainCVCell: UICollectionViewCell {
         setupConstraints()
         
     }
-}
-
-
-extension MainCVCell {
     
     func configure(with viewModel: ImageCellViewModel) {
         guard let img = viewModel.image else {return}
         self.imageView.image = img
-        let width = img.size.width
-        let height = img.size.height
-        let ratio = (width > height) ? width/height : height/width
-        let imageViewHeight = self.imageView.frame.size.height*ratio
-        let imageViewWidth = self.imageView.frame.size.width
-        let imageViewSize = CGSize(width: imageViewWidth, height: imageViewHeight)
-        self.imageView.frame.size = imageViewSize
-        
     }
     
     func setupConstraints() -> Void {
         
-//        self.imageView.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            self.imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
-//            self.imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
-//            self.imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
-//            self.imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-//        ])
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            imageView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
     }
 }
